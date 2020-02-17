cd6bd1ceaab8373af884ec13fefdebbed645c66f
xgrau <xavier.graubove@gmail.com>
2020-02-17 14:47:39 +0000

    plotphy

--- results_admixture_phylo/plot_phylo_04tip_2020-02-17.R
+++ results_admixture_phylo/plot_phylo_04tip_2020-02-17.R
@@ -5,7 +5,7 @@ library(ape)
 library(phytools)
 library(stringr)
 
-prefix    = "out"
+prefix    = "phylo"
 
 phy_list  = c("hapalignment_upstream.iqt.treefile",
               "hapalignment_duplication.iqt.treefile",
@@ -16,7 +16,7 @@ dups = read.table("haps_with_dups.csv", header = T)
 hap_has_dup = as.vector(dups[dups$has_dup,"hap"])
 
 #  plot phylogenies
-pdf(file=paste(prefix,"dists_phylo.pdf",sep="."),height=6,width=6)
+pdf(file=paste(prefix,"circol.pdf",sep="."),height=6,width=6)
 cou      = 0
 for (phy_fn in phy_list) {
   
@@ -30,7 +30,7 @@ for (phy_fn in phy_list) {
   phy$tip.species[phy$tip.species == ""] = "gamcol"
   phy$tip.population = word(phy$tip.label_sep,2)
   phy$tip.hasdup     = phy$tip.label %in% hap_has_dup
-  phy$tip.colfactor  = as.factor(paste(phy$tip.species,phy$tip.hasdup,sep="_"),sep="_")
+  phy$tip.colfactor  = as.factor(paste(phy$tip.species,phy$tip.hasdup,sep="_"))
   phy$tip.colors     = c("blue","turquoise3","orangered3","orange")[phy$tip.colfactor]
 
   # rename tips for nice plot
@@ -47,12 +47,14 @@ for (phy_fn in phy_list) {
              edge.color = "slategray3",
              font = 1, edge.width = 0.5, node.depth=1, cex=2,
              main=phy_name[cou])
+  ape::add.scale.bar(x=0, y=0, lcol="slategray", cex=0.8)
+  
 }
 
 dev.off()
 
 
-pdf(file=paste(prefix,"dists_phylo_llarg.pdf",sep="."),height=150,width=30)
+pdf(file=paste(prefix,"llarg.pdf",sep="."),height=150,width=30)
 cou      = 0
 for (phy_fn in phy_list) {
   
@@ -66,7 +68,7 @@ for (phy_fn in phy_list) {
   phy$tip.species[phy$tip.species == ""] = "gamcol"
   phy$tip.population = word(phy$tip.label_sep,2)
   phy$tip.hasdup     = phy$tip.label %in% hap_has_dup
-  phy$tip.colfactor  = as.factor(paste(phy$tip.species,phy$tip.hasdup,sep="_"),sep="_")
+  phy$tip.colfactor  = as.factor(paste(phy$tip.species,phy$tip.hasdup,sep="_"))
   phy$tip.colors     = c("blue","turquoise3","orangered3","orange")[phy$tip.colfactor]
   
   
@@ -77,6 +79,7 @@ for (phy_fn in phy_list) {
              edge.color = "slategray3",
              font = 1, edge.width = 0.5, node.depth=1, cex=1.2, underscore = T,
              main=phy_name[cou])
+  ape::add.scale.bar(x=0, y=0, lcol="slategray", cex=0.8)
   
 }
 
