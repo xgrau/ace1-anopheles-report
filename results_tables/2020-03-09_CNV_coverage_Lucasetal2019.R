efde392c3f47cb4cb014bdb6664819cea6fc77ad
xgrau <xavier.graubove@gmail.com>
2020-10-17 20:55:28 +0200

    hs boxplots along region, fix axes

--- results_tables/2020-03-09_CNV_coverage_Lucasetal2019.R
+++ results_tables/2020-03-09_CNV_coverage_Lucasetal2019.R
@@ -279,7 +279,7 @@ boxplot(dat_coverage, names = c("Ace1", tags), col="gray", ylab="Raw coverage", 
 #### INTERMISSION: PLOTS AND ECDFS FOR HAPLOTYPE SCORES ####
 
 hsd = read.table("dupcoverage_stats.HaplotypeScore.csv", sep="\t", header = T)
-ace_coverage = hsd[hsd$pos>ace_mut*1e6 - 300 & hsd$pos < ace_mut*1e6 + 300, "HaplotypeScore" ]
+ace_coverage = hsd[hsd$pos>ace_start*1e6 - 300 & hsd$pos < ace_end*1e6 + 300, "HaplotypeScore" ]
 plot(ecdf(ace_coverage), col="magenta", xlim=c(0,20), ylim=c(0,1), verticals = T, pch = NA,ylab="CDF", xlab="Raw coverage", main="HS, entire dataset")
 dat_coverage = list("Ace1" = ace_coverage)
 n=1
